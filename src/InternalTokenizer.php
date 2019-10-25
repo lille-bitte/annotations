6e664a5155c72726a5bdde34cc1adc971bf00892
plvhx <gandung@slackerc0de.us>
2019-10-25 13:21:05 +0000

    Adding InternalTokenizer.php..

---
+++ src/InternalTokenizer.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace LilleBitte\Annotations;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class InternalTokenizer implements TokenizerInterface
+{
+	/**
+	 * @var array|null
+	 */
+	private $token;
+
+	/**
+	 * @var array
+	 */
+	private $current;
+
+	/**
+	 * @var integer
+	 */
+	private $position;
+
+	public function __construct($contents)
+	{
+		$this->reset();
+		$this->parseToken($contents);
+	}
+
+	/**
+	 * Parse buffer input to a list of tokens.
+	 *
+	 * @param string $contents Buffer input.
+	 * @return void
+	 */
+	private function parseToken($contents)
+	{
+		$this->token = \token_get_all($contents);
+
+		foreach ($this->token as $key => $value) {
+			if (!is_array($value) || $value[0] === \T_WHITESPACE) {
+				unset($this->token[$key]);
+			}
+		}
+
+		$this->token = array_values($this->token);
+	}
+
+	/**
+	 * Reset tokenizer metadata.
+	 *
+	 * @return void
+	 */
+	private function reset()
+	{
+		$this->token = null;
+		$this->current = null;
+		$this->position = 0;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function next()
+	{
+		$this->current = !isset($this->token[$this->position])
+			? null
+			: $this->token[$this->position++];
+
+		return null !== $this->current;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getToken()
+	{
+		return $this->current;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getTokenType()
+	{
+		$token = $this->getToken();
+
+		return null === $token
+			? null
+			: $token[0];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getTokenValue()
+	{
+		$token = $this->getToken();
+
+		return null === $token
+			? null
+			: $token[1];
+	}
+}
