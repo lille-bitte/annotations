027f9b527b6bbe3063ea62a84ea40f448180fb52
plvhx <gandung@slackerc0de.us>
2019-10-25 13:23:32 +0000

    Adding functions.php..

---
+++ src/functions.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace LilleBitte\Annotations;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+
+/**
+ * Read contents of a file.
+ *
+ * @param string $file File name.
+ * @return string
+ */
+function readFile($file)
+{
+	if (!is_file($file)) {
+		throw AnnotationException::invalidArgument(
+			__FUNCTION__,
+			sprintf(
+				"file '%s' not exists.",
+				$file
+			)
+		);
+	}
+
+	$fobj = new \SplFileObject($file);
+
+	if (!($fobj instanceof \SplFileObject)) {
+		throw AnnotationException::invalidArgument(
+			__FUNCTION__,
+			sprintf(
+				"file '%s' has opened in wrong mode, or invalid permission.",
+				$file
+			)
+		);
+	}
+
+	$buf = '';
+
+	while (!$fobj->eof()) {
+		$buf .= $fobj->fgets();
+	}
+
+	return $buf;
+}
+
+/**
+ * Get list of a namespaces from
+ * a file.
+ *
+ * @param string $file File name.
+ * @return array
+ */
+function getNamespaces($file)
+{
+	$tokenizer = new InternalTokenizer(
+		readFile($file)
+	);
+
+	$res = [];
+
+	$callback = function() use ($tokenizer) {
+		$buf = '';
+
+		while ((false !== $tokenizer->next()) &&
+			   ($tokenizer->getTokenType() === T_STRING || $tokenizer->getTokenType() === T_NS_SEPARATOR)) {
+			$buf .= $tokenizer->getTokenValue();
+		}
+
+		return $buf;
+	};
+
+	// move to next token
+	$tokenizer->next();
+
+	while (null !== $tokenizer->getToken()) {
+		if ($tokenizer->getTokenType() === T_NAMESPACE) {
+			$res[] = $callback();
+			continue;
+		}
+
+		$tokenizer->next();
+	}
+
+	return $res;
+}
+
+/**
+ * Get list of a used class from
+ * a file.
+ *
+ * @param string $file File name.
+ * @return array
+ */
+function getClassUses($file)
+{
+	$tokenizer = new InternalTokenizer(
+		readFile($file)
+	);
+
+	$res = [];
+
+	$callback = function() use ($tokenizer) {
+		$ret = ['value' => '', 'alias' => null];
+
+		while ((false !== $tokenizer->next()) &&
+	           ($tokenizer->getTokenType() === T_STRING || $tokenizer->getTokenType() === T_NS_SEPARATOR ||
+	            $tokenizer->getTokenType() === T_AS)) {
+			if ($tokenizer->getTokenType() === T_AS) {
+				$tokenizer->next();
+
+				if ($tokenizer->getTokenType() !== T_STRING) {
+					throw AnnotationException::syntaxError(
+						__METHOD__,
+						"string"
+					);
+				}
+
+				$ret['alias'] = $tokenizer->getTokenValue();
+				continue;
+			}
+
+			$ret['value'] .= $tokenizer->getTokenValue();
+		}
+
+		return $ret;
+	};
+
+	// move to next token
+	$tokenizer->next();
+
+	while (null !== $tokenizer->getToken()) {
+		if ($tokenizer->getTokenType() === T_USE) {
+			$res[] = $callback();
+			continue;
+		}
+
+		$tokenizer->next();
+	}
+
+	return $res;
+}
