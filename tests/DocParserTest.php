37ac2ad09e2c4ca2090a6bced42958468584bf72
plvhx <gandung@slackerc0de.us>
2020-07-30 08:20:09 +0700

    Refactoring tests/DocParserTest.php

--- tests/DocParserTest.php
+++ tests/DocParserTest.php
@@ -3,8 +3,6 @@
 namespace LilleBitte\Annotations\Tests;
 
 use SplPriorityQueue;
-use Foo\Bar\Baz;
-use LilleBitte\Annotations\ClassRegistry;
 use LilleBitte\Annotations\DocLexer;
 use LilleBitte\Annotations\DocParser;
 use LilleBitte\Annotations\Exception\ClassNotExistsException;
@@ -25,70 +23,34 @@ class DocParserTest extends TestCase
 
     public function testCanResolveClassAlias()
     {
-        ClassRegistry::register(FooFixtures::class);
-
         $annotation = "/**\n * @FooFixtures\n */";
         $parser = new DocParser();
         $parser->setClassUses(
             \LilleBitte\Annotations\getClassUses(__FILE__)
         );
         $list = $parser->parse($annotation, sprintf("class %s", get_class($this)));
-
-        ClassRegistry::reset();
-
         $this->assertInstanceOf(FooFixtures::class, $list[0]->instance);
     }
 
     public function testCanResolveClassWithoutNamespace()
     {
-        ClassRegistry::register(SplPriorityQueue::class);
-
         $annotation = "/**\n * @SplPriorityQueue\n */";
         $parser = new DocParser();
         $parser->setClassUses(
             \LilleBitte\Annotations\getClassUses(__FILE__)
         );
         $list = $parser->parse($annotation, sprintf("class %s", get_class($this)));
-
-        ClassRegistry::reset();
-
         $this->assertInstanceOf(\SplPriorityQueue::class, $list[0]->instance);
-    }
-
-    /**
-     * @expectedException LilleBitte\Annotations\Exception\ClassNotExistsException
-     */
-    public function testCanThrowExceptionIfClassValueAnnotationNotExist()
-    {
-        ClassRegistry::register("Foo\\Bar\\Baz");
-
-        try {
-            $annotation = "/**\n * @Foo\\Bar\\Baz\n */";
-            $parser = new DocParser();
-            $parser->setClassUses(
-                \LilleBitte\Annotations\getClassUses(__FILE__)
-            );
-
-            $list = $parser->parse($annotation, sprintf("class %s", get_class($this)));
-        } catch (ClassNotExistsException $ce) {
-            ClassRegistry::reset();
-            throw $ce;
-        }
     }
 
     public function testCanGetResolveClassWithEmptyParameters()
     {
-        ClassRegistry::register(FooFixtures::class);
-
         $annotation = "/**\n * @FooFixtures()\n */";
         $parser = new DocParser();
         $parser->setClassUses(
             \LilleBitte\Annotations\getClassUses(__FILE__)
         );
         $list = $parser->parse($annotation, sprintf("class %s", get_class($this)));
-
-        ClassRegistry::reset();
-
         $this->assertInstanceOf(FooFixtures::class, $list[0]->instance);
     }
 
@@ -103,8 +65,6 @@ class DocParserTest extends TestCase
 
     public function testCanParseClassValueConstructorArguments()
     {
-        ClassRegistry::register(FooFixtures::class);
-
         $annotation = "/**\n * @FooFixtures(10, -1, \"a\", +1, -1.1, +1.1, a=10, true, " .
             "false, {\"x\", \"y\"},)\n */";
         $parser = new DocParser();
@@ -112,9 +72,6 @@ class DocParserTest extends TestCase
             \LilleBitte\Annotations\getClassUses(__FILE__)
         );
         $list = $parser->parse($annotation, sprintf("class %s", get_class($this)));
-
-        ClassRegistry::reset();
-
         $this->assertInstanceOf(FooFixtures::class, $list[0]->instance);
     }
 
@@ -123,8 +80,6 @@ class DocParserTest extends TestCase
      */
     public function testCanThrowExceptionWhileParseClassValueConstructorArguments()
     {
-        ClassRegistry::register(FooFixtures::class);
-
         try {
             $annotation = "/**\n * @FooFixtures(x, y)\n */";
             $parser = new DocParser();
@@ -133,40 +88,29 @@ class DocParserTest extends TestCase
             );
             $parser->parse($annotation, sprintf("class %s", get_class($this)));
         } catch (\LilleBitte\Annotations\Exception\SyntaxErrorException $se) {
-            ClassRegistry::reset();
             throw $se;
         }
     }
 
     public function testCanParseEmptyArray()
     {
-        ClassRegistry::register(FooFixtures::class);
-
         $annotation = "/**\n * @FooFixtures({})\n */";
         $parser = new DocParser();
         $parser->setClassUses(
             \LilleBitte\Annotations\getClassUses(__FILE__)
         );
         $list = $parser->parse($annotation, sprintf("class %s", get_class($this)));
-
-        ClassRegistry::reset();
-
         $this->assertInstanceOf(FooFixtures::class, $list[0]->instance);
     }
 
     public function testCanParseArrayWithTrailingComma()
     {
-        ClassRegistry::register(FooFixtures::class);
-
         $annotation = "/**\n * @FooFixtures({10,})\n */";
         $parser = new DocParser();
         $parser->setClassUses(
             \LilleBitte\Annotations\getClassUses(__FILE__)
         );
         $list = $parser->parse($annotation, sprintf("class %s", get_class($this)));
-
-        ClassRegistry::reset();
-
         $this->assertInstanceOf(FooFixtures::class, $list[0]->instance);
     }
 
@@ -175,8 +119,6 @@ class DocParserTest extends TestCase
      */
     public function testCanThrowExceptionWhileTokenAssertionHasFailed()
     {
-        ClassRegistry::register(FooFixtures::class);
-
         try {
             $annotation = "/**\n * @FooFixtures(10\n */";
             $parser = new DocParser();
@@ -185,7 +127,6 @@ class DocParserTest extends TestCase
             );
             $parser->parse($annotation, sprintf("class %s", get_class($this)));
         } catch (\LilleBitte\Annotations\Exception\SyntaxErrorException $se) {
-            ClassRegistry::reset();
             throw $se;
         }
     }
