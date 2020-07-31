320471b87b8c62c9e85425734e67a093c29daa28
plvhx <gandung@slackerc0de.us>
2020-07-31 17:24:41 +0700

    Refactoring src/PhpFileParser.php

--- src/PhpFileParser.php
+++ src/PhpFileParser.php
@@ -15,159 +15,159 @@ use const T_USE;
  */
 final class PhpFileParser
 {
-	/**
-	 * @var TokenizerInterface
-	 */
-	private $tokenizer;
+    /**
+     * @var TokenizerInterface
+     */
+    private $tokenizer;
 
-	public function __construct(TokenizerInterface $tokenizer = null)
-	{
-		$this->tokenizer = $tokenizer ?? new InternalTokenizer();
-	}
+    public function __construct(TokenizerInterface $tokenizer = null)
+    {
+        $this->tokenizer = $tokenizer ?? new InternalTokenizer();
+    }
 
-	/**
-	 * Get all namespaces in current PHP file.
-	 *
-	 * @param string $file PHP file name.
-	 * @return array
-	 */
-	public function getNamespaces($file)
-	{
-		$res = [];
+    /**
+     * Get all namespaces in current PHP file.
+     *
+     * @param string $file PHP file name.
+     * @return array
+     */
+    public function getNamespaces($file)
+    {
+        $res = [];
 
-		$this->tokenizer->setContent($this->readFile($file));
-		$this->tokenizer->refresh();
-		$this->tokenizer->next();
+        $this->tokenizer->setContent($this->readFile($file));
+        $this->tokenizer->refresh();
+        $this->tokenizer->next();
 
-		while (null !== $this->tokenizer->getToken()) {
-			if ($this->tokenizer->getTokenType() === T_NAMESPACE) {
-				$res[] = $this->getNamespaceFromCurrentTokenizer($this->tokenizer);
-				continue;
-			}
+        while (null !== $this->tokenizer->getToken()) {
+            if ($this->tokenizer->getTokenType() === T_NAMESPACE) {
+                $res[] = $this->getNamespaceFromCurrentTokenizer($this->tokenizer);
+                continue;
+            }
 
-			$this->tokenizer->next();
-		}
+            $this->tokenizer->next();
+        }
 
-		return $res;
-	}
+        return $res;
+    }
 
-	/**
-	 * Get all class uses in current PHP file.
-	 *
-	 * @param string $file PHP file name.
-	 * @return array
-	 */
-	public function getClassUses($file)
-	{
-		$res = [];
+    /**
+     * Get all class uses in current PHP file.
+     *
+     * @param string $file PHP file name.
+     * @return array
+     */
+    public function getClassUses($file)
+    {
+        $res = [];
 
-		$this->tokenizer->setContent($this->readFile($file));
-		$this->tokenizer->refresh();
-		$this->tokenizer->next();
+        $this->tokenizer->setContent($this->readFile($file));
+        $this->tokenizer->refresh();
+        $this->tokenizer->next();
 
-		while (null !== $this->tokenizer->getToken()) {
-			if ($this->tokenizer->getTokenType() === T_USE) {
-				$res[] = $this->getClassUsesFromCurrentTokenizer($this->tokenizer);
-				continue;
-			}
+        while (null !== $this->tokenizer->getToken()) {
+            if ($this->tokenizer->getTokenType() === T_USE) {
+                $res[] = $this->getClassUsesFromCurrentTokenizer($this->tokenizer);
+                continue;
+            }
 
-			$this->tokenizer->next();
-		}
+            $this->tokenizer->next();
+        }
 
-		return $res;
-	}
+        return $res;
+    }
 
-	/**
-	 * Read contents of a PHP file.
-	 *
-	 * @param string $file PHP file name.
-	 * @return string
-	 */
-	private function readFile($file)
-	{
-		if (!is_file($file)) {
-			throw AnnotationException::invalidArgument(
-				__FUNCTION__,
-				sprintf("File '%s' not exists.", $file)
-			);
-		}
+    /**
+     * Read contents of a PHP file.
+     *
+     * @param string $file PHP file name.
+     * @return string
+     */
+    private function readFile($file)
+    {
+        if (!is_file($file)) {
+            throw AnnotationException::invalidArgument(
+                __FUNCTION__,
+                sprintf("File '%s' not exists.", $file)
+            );
+        }
 
-		$fileObject = new \SplFileObject($file);
+        $fileObject = new \SplFileObject($file);
 
-		if (!is_a($fileObject, \SplFileObject::class)) {
-			throw AnnotationException::invalidArgument(
-				__FUNCTION__,
-				sprintf(
-					"File '%s' has opened in write-only mode, or invalid permission.",
-					$file
-				)
-			);
-		}
+        if (!is_a($fileObject, \SplFileObject::class)) {
+            throw AnnotationException::invalidArgument(
+                __FUNCTION__,
+                sprintf(
+                    "File '%s' has opened in write-only mode, or invalid permission.",
+                    $file
+                )
+            );
+        }
 
-		$buf = '';
+        $buf = '';
 
-		while (!$fileObject->eof()) {
-			$buf .= $fileObject->fgets();
-		}
+        while (!$fileObject->eof()) {
+            $buf .= $fileObject->fgets();
+        }
 
-		return $buf;
-	}
+        return $buf;
+    }
 
-	/**
-	 * Get single namespace from given tokenizer object.
-	 *
-	 * @param TokenizerInterface $tokenizer Tokenizer object.
-	 * @return string
-	 */
-	private function getNamespaceFromCurrentTokenizer(TokenizerInterface $tokenizer)
-	{
-		$buf = '';
+    /**
+     * Get single namespace from given tokenizer object.
+     *
+     * @param TokenizerInterface $tokenizer Tokenizer object.
+     * @return string
+     */
+    private function getNamespaceFromCurrentTokenizer(TokenizerInterface $tokenizer)
+    {
+        $buf = '';
 
-		while ((false !== $tokenizer->next()) &&
-	           ($tokenizer->getTokenType() === T_STRING || $tokenizer->getTokenType() === T_NS_SEPARATOR)) {
-			$buf .= $tokenizer->getTokenValue();
-		}
+        while ((false !== $tokenizer->next()) &&
+               ($tokenizer->getTokenType() === T_STRING || $tokenizer->getTokenType() === T_NS_SEPARATOR)) {
+            $buf .= $tokenizer->getTokenValue();
+        }
 
-		return $buf;
-	}
+        return $buf;
+    }
 
-	/**
-	 * Get single class uses from given tokenizer object.
-	 *
-	 * @param TokenizerInterface $tokenizer Tokenizer object.
-	 * @return array
-	 */
-	private function getClassUsesFromCurrentTokenizer(TokenizerInterface $tokenizer)
-	{
-		$retVal = [
-			'value' => '',
-			'class' => '',
-			'alias' => ''
-		];
+    /**
+     * Get single class uses from given tokenizer object.
+     *
+     * @param TokenizerInterface $tokenizer Tokenizer object.
+     * @return array
+     */
+    private function getClassUsesFromCurrentTokenizer(TokenizerInterface $tokenizer)
+    {
+        $retVal = [
+            'value' => '',
+            'class' => '',
+            'alias' => ''
+        ];
 
-		while ((false !== $tokenizer->next()) &&
-	           ($tokenizer->getTokenType() === T_STRING || $tokenizer->getTokenType() === T_NS_SEPARATOR ||
-	            $tokenizer->getTokenType() === T_AS)) {
-			if ($tokenizer->getTokenType() === T_AS) {
-				$tokenizer->next();
+        while ((false !== $tokenizer->next()) &&
+               ($tokenizer->getTokenType() === T_STRING || $tokenizer->getTokenType() === T_NS_SEPARATOR ||
+                $tokenizer->getTokenType() === T_AS)) {
+            if ($tokenizer->getTokenType() === T_AS) {
+                $tokenizer->next();
 
-				if ($tokenizer->getTokenType() !== T_STRING) {
-					throw AnnotationException::syntaxError(
-						__METHOD__,
-						"string"
-					);
-				}
+                if ($tokenizer->getTokenType() !== T_STRING) {
+                    throw AnnotationException::syntaxError(
+                        __METHOD__,
+                        "string"
+                    );
+                }
 
-				$retVal['alias'] = $tokenizer->getTokenValue();
-				$retVal['class'] = '';
-				continue;
-			}
+                $retVal['alias'] = $tokenizer->getTokenValue();
+                $retVal['class'] = '';
+                continue;
+            }
 
-			$tokenValue       = $tokenizer->getTokenValue();
-			$retVal['value'] .= $tokenValue;
-			$retVal['class']  = $tokenValue;
-		}
+            $tokenValue       = $tokenizer->getTokenValue();
+            $retVal['value'] .= $tokenValue;
+            $retVal['class']  = $tokenValue;
+        }
 
-		return $retVal;
-	}
+        return $retVal;
+    }
 }
